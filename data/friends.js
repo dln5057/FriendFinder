468ddd8d0d180dc7fae4f8f99a0c6b82699fbdf1
Donnie Nowels <djnowels11@gmail.com>
2016-12-06 19:26:01 -0500

    made new repository and added files I had already to new repository

---
+++ data/friends.js
@@ -0,0 +1,15 @@
+var friendsList = [{
+  name: 'Dwanyne "The Rock" Johnson',
+  photo: 'https://s-media-cache-ak0.pinimg.com/736x/d9/90/90/d99090062d42bd90a9a5660cebfa4262.jpg',
+  scores: [5,1,2,2,2,1,3,4,1,3],
+}, {
+  name: 'Kevin Hart',
+  photo: 'http://media.philly.com/images/kevin-hart-live-getty-600.jpg',
+  scores: [1,5,3,5,4,5,2,5,1,3],
+}, {
+  name: 'Chuck Norris',
+  photo: 'http://cdn.business2community.com/wp-content/uploads/2016/03/Vd3MJo.jpg',
+  scores: [5,5,1,3,4,3,2,3,1,5],
+}];
+
+module.exports = friendsList;
